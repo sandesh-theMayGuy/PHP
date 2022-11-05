29431956e95822ff1d43c46e4c438739a8544506
Sandesh <khatiwadasandesh01@gmail.com>
2022-11-05 23:28:48 +0545

    Add files via upload

---
+++ 2.variables_and_constants.php
@@ -0,0 +1,19 @@
+
+<?php
+//variables in PHP 
+
+$name = "Sandesh";
+$age = 20;
+echo $name;
+echo $age;
+
+// constants in PHP 
+
+define("ADDRESS","Ilam");
+echo ADDRESS;
+
+
+
+
+?>
+
---
+++ 3.strings.php
@@ -0,0 +1,13 @@
+<?php
+
+$string1 = 'My name is ';
+$string2 = 'Sandesh';
+
+
+echo "Hey its me " . $string2;   //string concatenation
+
+echo " \nHello I am $string2 .";    //variable interpolation
+
+echo $string1[0];  //indexing
+
+?>
